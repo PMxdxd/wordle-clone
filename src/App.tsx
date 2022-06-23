55c3ede634a3508f098ae79930718b5969d6e1cd
PM____ <40933856+PMxdxd@users.noreply.github.com>
2022-06-24 04:04:36 +0900

    画面からの入力実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Keyboard } from "./components/keyboard/Keyboard";
 import { MAX_WORD_LENGTH, MAX_CHALLENGES } from "./constants/settings";
 import "./App.css";
@@ -9,22 +9,50 @@ function App() {
   const [guesses, setGuesses] = useState<string[]>([]);
 
   const onChar = (value: String) => {
-    if (`${currentGuess}${value}`.length <= MAX_WORD_LENGTH) {
-      setCurrentGuess(`${currentGuess}${value}`);
-    }
+    setCurrentGuess((prev) => {
+      if (`${prev}${value}`.length <= MAX_WORD_LENGTH) {
+        return `${prev}${value}`;
+      } else {
+        return prev;
+      }
+    });
   };
 
   const onEnter = () => {
     //答えをセット
     if (currentGuess.length === MAX_WORD_LENGTH) {
-      setGuesses([...guesses, currentGuess]);
+      setGuesses((prev) => [...prev, currentGuess]);
       setCurrentGuess("");
     }
   };
 
   const onDelete = () => {
-    setCurrentGuess(currentGuess.slice(0, -1));
+    setCurrentGuess((prev) => prev.slice(0, -1));
   };
+
+  const keydownFunc = useCallback(
+    (e: KeyboardEvent) => {
+      const { key } = e;
+      console.log(key)
+      if (key === "Backspace") onDelete();
+      if (key === "Enter") onEnter();
+
+      const upperkey = key.toUpperCase();
+      const canTypeKey =
+        (key.length === 1 && upperkey >= "A" && upperkey <= "Z") ||
+        ("0" <= key && "9" >= key) ||
+        key === "-";
+      if (canTypeKey) onChar(upperkey);
+    },
+    [currentGuess]
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", keydownFunc);
+    return () => {
+      window.removeEventListener("keydown", keydownFunc);
+    };
+  }, [keydownFunc]);
 
   return (
     <div className="App">
