1170b1a49d8ab857befcdee7fb58a620adead97b
PM____ <40933856+PMxdxd@users.noreply.github.com>
2022-07-06 01:46:21 +0900

    Toast実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+/* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { useCallback, useEffect, useState } from "react";
 import { Keyboard } from "./components/keyboard/Keyboard";
-import { MAX_WORD_LENGTH, MAX_CHALLENGES } from "./constants/settings";
+import {
+  MAX_WORD_LENGTH,
+  MAX_CHALLENGES,
+  NOT_ENOUGH_LETTERS_MESSAGE,
+  WORD_NOT_FOUND_MESSAGE,
+} from "./constants/settings";
 import "./App.css";
 import { Board } from "./components/board/Board";
 import { isWinningWord, solution } from "./lib/words";
 import { Result } from "./components/modal/Result";
+import { Snackbar } from "@mui/material";
+import { Toast } from "./components/feedback/Toast";
+import { WORDS } from "./constants/wordlist";
 
 function App() {
   const [currentGuess, setCurrentGuess] = useState("");
   const [guesses, setGuesses] = useState<string[]>([]);
   const [isGameWon, setIsGameWon] = useState(false);
+  const [isGameLost, setIsGameLost] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
   const onChar = (value: String) => {
@@ -27,8 +39,18 @@ function App() {
   };
 
   const onEnter = () => {
-    if (isGameWon) {
+    if (isGameWon || isGameLost) {
       return;
+    }
+
+    //文字数が足らない
+    if (currentGuess.length !== MAX_WORD_LENGTH) {
+      return handleToastOpen(NOT_ENOUGH_LETTERS_MESSAGE);
+    }
+
+    //存在しないワード
+    if (!WORDS.includes(currentGuess.toLowerCase())) {
+      return handleToastOpen(WORD_NOT_FOUND_MESSAGE);
     }
 
     //答えをセット
@@ -40,6 +62,11 @@ function App() {
     // setIsGameWon(isWinningWord(currentGuess));
     const winningWord = isWinningWord(currentGuess);
     if (winningWord) {
+      setIsGameWon(true);
+    }
+
+    if (guesses.length >= MAX_CHALLENGES - 1) {
+      return handleToastOpen(solution,5000);
     }
   };
 
@@ -70,11 +97,35 @@ function App() {
     };
   }, [keydownFunc]);
 
-  
-  const handleModal = () => {
-    setShowModal(prev => !prev);
+  const [toastInfo, setToastInfo] = useState({
+    open: false,
+    toastMessage: "",
+    autoHideDuration: 1500,
+  });
+  const { open, toastMessage, autoHideDuration } = toastInfo;
+
+  const handleToastOpen = (
+    message: string,
+    autoHideDuration: number = 1500
+  ) => {
+    setToastInfo({
+      open: true,
+      toastMessage: message,
+      autoHideDuration: autoHideDuration,
+    });
   };
-  
+
+  const handleToastClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setToastInfo({ ...toastInfo, open: false });
+  };
+
   return (
     <div className="App">
       <header className="App-header"></header>
@@ -88,7 +139,13 @@ function App() {
           gusses={guesses}
         />
       </div>
-      <Result showModal={showModal} closeModal={handleModal}></Result>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={open}
+        autoHideDuration={autoHideDuration}
+        onClose={handleToastClose}
+        message={toastMessage}
+      />
     </div>
   );
 }
--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -2,3 +2,5 @@ export const MAX_WORD_LENGTH = 5
 export const MAX_CHALLENGES = 6
 
 export const WIN_MESSAGES = 'Great Job!'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
+export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
